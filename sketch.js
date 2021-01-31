2f044a7ea136cf534a614ee4f5cf1d3c15b25fb6
Ashnasmitha <75996347+Ashnasmitha@users.noreply.github.com>
2021-01-31 17:55:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-var dog,sadDog,happyDog;
-var foodObj,foods,foodStock;
-var fedTime,lastFed,feed,addFood;
+var dog,sadDog,happyDog, database;
+var foods,foodStock;
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
 
 function preload(){
-  sadDog=loadImage("Images/Dog.png");
-  happyDog=loadImage("Images/happy dog.png");
+sadDog=loadImage("Images/Dog.png");
+happyDog=loadImage("Images/happy dog.png");
 }
 
 function setup() {
@@ -17,14 +19,14 @@ function setup() {
   dog.addImage(sadDog);
   dog.scale=0.15;
 
-  foodStock = database.ref('Food');
+  foodStock=database.ref('Food');
   foodStock.on("value",readStock);
-
-  feed = createButton("Feed The Dog");
+  
+  feed=createButton("Feed the dog");
   feed.position(700,95);
   feed.mousePressed(feedDog);
 
-  addFood = createButton("Add Food");
+  addFood=createButton("Add Food");
   addFood.position(800,95);
   addFood.mousePressed(addFoods);
 
@@ -32,27 +34,23 @@ function setup() {
 
 function draw() {
   background(46,139,87);
-
   foodObj.display();
 
   fedTime=database.ref('FeedTime');
   fedTime.on("value",function(data){
     lastFed=data.val();
-  })
-
+  });
+ 
   fill(255,255,254);
   textSize(15);
-
   if(lastFed>=12){
-    text("Last Feed:"+ lastFed %12 +"PM",350,30);
-  }
-  else if(lastFed == 0){
-    text("Last Feed: 12AM",350,30);
-  }
-  else {
-    text("Last Feed:"+ lastFed + "AM",350,30)
-  }
-
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
+ 
   drawSprites();
 }
 
@@ -66,9 +64,14 @@ function readStock(data){
 //function to update food stock and last fed time
 function feedDog(){
   dog.addImage(happyDog);
-
-  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
-  databse.ref('/').update({
+  
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  
+  database.ref('/').update({
     Food:foodObj.getFoodStock(),
     FeedTime:hour()
   })
@@ -80,12 +83,4 @@ function addFoods(){
   database.ref('/').update({
     Food:foods
   })
-}
-
-async function hour(){
-  var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-  var responseJSON=await response.json();
-  var datetime=responseJSON.datetime;
-  var hour = datetime.slice(11,13);
-  return hour;
 }
